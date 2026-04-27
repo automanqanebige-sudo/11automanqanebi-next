a9a2b8260ab7f28f5fd7fc5ba97ae77373a57e68
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-04-27 18:59:29 +0400

    Update route.ts

--- app/api/cars/route.ts
+++ app/api/cars/route.ts
@@ -1,1 +1,9 @@
-1
+export async function GET() {
+  const cars = [
+    { id: 1, name: "BMW X5", price: 15000 },
+    { id: 2, name: "Mercedes E-Class", price: 13000 },
+    { id: 3, name: "Toyota Camry", price: 10000 },
+  ];
+
+  return Response.json(cars);
+}
